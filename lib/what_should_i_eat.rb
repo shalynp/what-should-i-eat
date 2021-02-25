833b70711358f10bee3d8ba796866ae5e2a0af07
Shalyn-p <shalynxp3@gmail.com>
2021-02-25 12:38:53 -0700

    bundle installed rake and rubocop

--- lib/what_should_i_eat.rb
+++ lib/what_should_i_eat.rb
@@ -1,8 +1,20 @@
 # frozen_string_literal: true
+require "nokogiri"
+require "open_uri"
 
 require_relative "what_should_i_eat/version"
 
 module WhatShouldIEat
-  class Error < StandardError; end
-  # Your code goes here...
-end
+  class RecipeChooser
+
+    doc = Nokogiri::HTML(URI.open('https://www.edamam.com/search?type=Feeds'))
+
+    search = doc.css(".label")
+    puts search
+
+    # list = doc.css(".ingredient")
+    # list.each do |n|
+    #   puts n.inner_html
+    end
+
+  end
